6d39f45c47b8b61c7194f44a4026042dbbeaeeef
Impact Design | Fashionable Website | Smart Code <ozellamaryleone@gmail.com>
2024-07-31 18:53:30 +0300

    Update Loading.jsx

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -3,7 +3,7 @@ import "../../src/components/Loading.css";
 export default function Loading() {
     return (
         <div className="text-center">
-        <div className="lds-facebook"><div></div><div></div><div></div></div>
+        
         </div>
     )
 }
