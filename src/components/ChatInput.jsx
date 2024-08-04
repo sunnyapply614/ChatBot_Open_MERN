a19937f2ed394e4efa88de75194b6b4c3bd1e572
Impact Design | Fashionable Website | Smart Code <ozellamaryleone@gmail.com>
2024-08-04 19:55:31 +0300

    Update ChatInput.jsx

--- src/components/ChatInput.jsx
+++ src/components/ChatInput.jsx
@@ -28,13 +28,7 @@ export default function ChatInput({ disabled, onSend, setIsCopyAnswer }) {
             }}
             autoComplete="off"
         >
-            <input
-                name="message"
-                placeholder="Type your message"
-                className="w-full px-4 block bg-transparent inputResult"
-                value={message}
-                onChange={(e) => setMessage(e.target.value)}
-            />
+            
 //send button
             <button
                 disabled={disabled}
