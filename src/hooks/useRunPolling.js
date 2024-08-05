eaf532ddfa904f78af5a576a98c9c4a5d3cba21d
Impact Design | Fashionable Website | Smart Code <ozellamaryleone@gmail.com>
2024-08-05 10:45:22 +0300

    Update useRunPolling.js

--- src/hooks/useRunPolling.js
+++ src/hooks/useRunPolling.js
@@ -5,7 +5,14 @@ import {runFinishedStates} from "../hooks/constants";
 export const useRunPolling = (threadId, run, setRun) => {
     const pollingTimerRef = useRef(null);
 
-    
+    const startPolling = async () => {
+        console.log(`Polling thread ${threadId} run ${run.run_id}`);
+        const data = await fetchRun(threadId, run.run_id);
+        if (data.run_id !== run.run_id || data.status !== run.status) {
+            setRun(data);
+        }
+        pollingTimerRef.current = setTimeout(startPolling, 1000);
+    };
 
     const stopPolling = () => clearInterval(pollingTimerRef.current);
 
